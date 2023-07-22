9eaa354a84d87ec2b1e5e8972d6f3ec4c810a228
M1nsong-Kim <lgms.3222@gmail.com>
2023-07-22 16:04:04 +0900

    프로젝트 생성

---
+++ src/main/java/hello/practicemsg/PracticeMsgApplication.java
@@ -0,0 +1,13 @@
+package hello.practicemsg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeMsgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeMsgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hello/practicemsg/PracticeMsgApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.practicemsg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeMsgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
