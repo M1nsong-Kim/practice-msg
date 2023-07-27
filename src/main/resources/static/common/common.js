28fe1096c354e4c9b281bf3c3160fdb98b110cc7
M1nsong-Kim <lgms.3222@gmail.com>
2023-07-27 22:19:03 +0900

    알고리즘 기본 뼈대

--- src/main/resources/static/common/common.js
+++ src/main/resources/static/common/common.js
@@ -2,7 +2,20 @@ var msg = [];	// 객체 배열
 
 // 입력한 메시지 창 화면에 띄우기
 function msgOnBoard(){
+	var speaker = $(":input:radio[name=speaker]:checked").val();
+	var content = $('#content').val();
 	
+	console.log(speaker);
+	console.log(content);
+	
+	if(speaker == 0){	// 나 -> 오른쪽
+		
+	}else{
+		
+	}
+	
+	// 다 하고 text창 비우기
+	$('#content').val('');
 }
 
 // 저장 전 이미지 확인 팝업
