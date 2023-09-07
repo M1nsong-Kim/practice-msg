531b17491d959d8f62c926e2a6479997fcbc2b9e
M1nsong-Kim <lgms.3222@gmail.com>
2023-09-07 21:23:57 +0900

    popup: 초대자 함수 추가

--- src/main/resources/static/popup.js
+++ src/main/resources/static/popup.js
@@ -2,6 +2,7 @@ getFriendList();
 
 function getFriendList(){
 	var code = "";
+	var code2 = "";
 	$.ajax({
 		url: '/getFriendList'
 		, method: 'POST'
@@ -11,22 +12,33 @@ function getFriendList(){
 			alert('success : ' + data);
 			console.log(data);
 			for(let i = 0; i < data.length; i++){
-				code += "<input type='checkbox'>" + data[i].name;
+				// 수정 필요
+				code += "<input type='radio' id='" + data[i].name + "' onclick='exceptInviter(this)'>" + data[i].name;
+				code2 += "<input type='checkbox' name='friend' value='" + data[i].name + "'>" + data[i].name;
 			}
 		}
 		, error: function(data){
 			alert('error : ' + data);
 		}
 	});
-	console.log(code);
-	$('.friendList').append(code);	// 수정 필요
+	// 수정 필요
+	$('.friendList').append(code);
+	$('.friendList2').append(code2);	
+}
+
+function exceptInviter(e){
+	$('input:checkbox[name=friend]:checked').each(function(){
+		if(this.val() == $(e).attr('id')){
+			$(this).prop('disabled', true);
+		}
+	})
 }
 
 function getInNOutMsg(e){
 	var idx = $(e).attr('name');
 	if(idx == 2){	// 퇴장
 		// 퇴장은 한 명씩만
-		if($('input:checkbox').length != 1){
+		if($('input:checkbox[name=friend]:checked').length != 1){
 		return;
 		};
 	}else{	// 입장
